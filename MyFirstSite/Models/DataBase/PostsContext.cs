7d5061db3b7a053046620cb645b00d45daa5f282
C4e10VeK <tank.t3445@gmail.com>
2020-09-14 15:59:57 +1000

    add project

---
+++ MyFirstSite/Models/DataBase/PostsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SiteKustiX.Models.DataBase
+{
+    //сама магия для работы и вывода даных из таблицы
+    public sealed class PostsContext : DbContext
+    {
+        public DbSet<Posts> Postses { get; set; }
+
+        public PostsContext(DbContextOptions<PostsContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
